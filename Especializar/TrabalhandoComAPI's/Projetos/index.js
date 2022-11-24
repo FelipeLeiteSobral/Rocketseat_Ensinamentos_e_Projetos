b4d9e2c6f4396d2cd11ad65337e601740c88abdc
Felipe Leite Sobral <felipeleitesobral@hotmail.com>
2022-11-24 10:30:39 -0300

    server commands

--- Especializar/TrabalhandoComAPI's/Projetos/index.js
+++ Especializar/TrabalhandoComAPI's/Projetos/index.js
@@ -1,4 +1,28 @@
+const { response } = require('express')
 const express = require('express')
+
 const app = express()  
 
 app.listen("3000")
+
+//GET
+app.route('/').get((req,res)=>res.send(author))
+
+app.route('/sobre').get((req,res)=>res.send("Hello sobre"))
+
+//POST
+//middleware
+app.use(express.json())
+
+app.route('/').post((req,res)=> res.send(req.body))
+
+//PUT
+//middleware
+app.use(express.json())
+
+let author = "Felipy"
+
+app.route("/").put((req,res)=> {
+    author = req.body.author //coloca-se author após o ponto para ele receber apenas o conteúdo dentro da chave e não a estrutura de JSON inteira
+    res.send(author)
+})
